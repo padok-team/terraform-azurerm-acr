965f1b9ddade63c4477a8d1175e0161fafe41a88
Blouni <philippe.dupart93@gmail.com>
2023-01-06 18:50:23 +0100

    feat: update provider version and fixing exemple

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ Terraform module which creates **Container Registry** resources on **Azure**. Th
 
 ```hcl
 module "acr" {
-  source = "https://github.com/padok-team/terraform-azurerm-acr?ref=v0.1.0"
+  source = "https://github.com/padok-team/terraform-azurerm-acr"
 
   name                = "test-acr"
   resource_group_name = "test-acr"
--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -5,16 +5,16 @@ resource "random_pet" "acr" {
   separator = ""
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "this" {
   name     = "example-acr"
   location = "francecentral"
 }
 
 module "key_vault" {
-  source = "git@github.com:padok-team/terraform-azurerm-keyvault.git?ref=v0.2.0"
+  source = "git@github.com:padok-team/terraform-azurerm-keyvault.git?ref=v0.4.0"
 
   name           = random_pet.keyvault.id # KeyVault names are globally unique
-  resource_group = azurerm_resource_group.example
+  resource_group = azurerm_resource_group.this
   tenant_id      = data.azurerm_client_config.this.tenant_id
   sku_name       = "standard"
 
@@ -22,7 +22,7 @@ module "key_vault" {
   enable_rbac_authorization = true
 
   depends_on = [
-    azurerm_resource_group.example
+    azurerm_resource_group.this
   ]
 }
 
@@ -32,7 +32,7 @@ resource "azurerm_role_assignment" "keyvault_administrator" {
   principal_id         = "UPDATE-ME !!!" # TODO: set your principal ID here
 }
 
-resource "azurerm_key_vault_key" "example" {
+resource "azurerm_key_vault_key" "this" {
   name         = "acr-key"
   key_vault_id = module.key_vault.this.id
   key_type     = "RSA"
@@ -54,15 +54,14 @@ resource "azurerm_key_vault_key" "example" {
 }
 
 module "acr" {
-  source              = "../.."
-  name                = random_pet.acr.id
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  source         = "../.."
+  name           = random_pet.acr.id
+  resource_group = azurerm_resource_group.this
 
   public_network_access_enabled = true
 
   encryption_key_vault_id     = module.key_vault.this.id
-  encryption_key_vault_key_id = azurerm_key_vault_key.example.id
+  encryption_key_vault_key_id = azurerm_key_vault_key.this.id
 
   depends_on = [
     module.key_vault
